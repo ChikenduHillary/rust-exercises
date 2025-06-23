19e35a3739dd3bf709dc588daddcdd9e4262b08b
ChikenduHillary <chikenduhillarychiagoziemu@gmail.com>
2025-06-23 12:58:08 +0100

    turples

--- src/bin/a9.rs
+++ src/bin/a9.rs
@@ -9,4 +9,18 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn get_cordinates() -> (i32, i32) {
+    (2390, 5)
+}
+
+fn main() {
+    let (_x, y) = get_cordinates();
+
+    if y > 5 {
+        println!("y cordinate is greater than 5");
+    } else if y < 5 {
+        println!("y cordinate is less than 5");
+    } else {
+        println!("y cordinate is equal 5");
+    }
+}
