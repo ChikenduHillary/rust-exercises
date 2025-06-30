128165a98aa4349ef518de969f313d50bf9bbc0a
ChikenduHillary <cikenduhill@gmail.com>
2025-06-30 10:05:21 +0100

    impl keyword and vec! macro

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -10,4 +10,59 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+struct Dimensions {
+    width: f64,
+    height: f64,
+    breath: f64
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("width={:?}", self.width);
+        println!("height={:?}", self.height);
+        println!("breath={:?}", self.breath);
+    }
+}
+
+enum Color {
+    Red,
+    Blue,
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Red => println!("Red"),
+            Color::Blue => println!("Blue")
+        }
+    }
+}
+
+struct ShippingBox {
+    dimensions: Dimensions,
+    color: Color,
+    weight: f64,
+}
+
+impl ShippingBox {
+    fn new(dimensions: Dimensions, color: Color, weight: f64) -> Self {
+        Self {dimensions, color, weight}
+    }
+
+    fn print(&self) {
+        println!("weight={:?}", self.weight);
+        self.color.print();
+        self.dimensions.print();
+    }
+}
+
+fn main() {
+    let small_box = Dimensions {
+        height: 33.3,
+        width: 23.4,
+        breath: 35.6
+    };
+
+    let new_box = ShippingBox::new(small_box, Color::Red, 50.0);
+    new_box.print();
+}
--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,4 +10,16 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    let numbers = vec![10, 20, 30, 40];
+
+    for num in &numbers {
+        if *num == 30 {
+            println!("thirty");
+        } else {
+            println!("{:?}", num);
+        }
+    }
+
+    println!("lenght of numbers vec is {:?}", numbers.len());
+}
