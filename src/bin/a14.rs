c533d9bf405263e337b0239e1fe38172e47e7615
ChikenduHillary <cikenduhill@gmail.com>
2025-08-22 15:29:01 +0100

    learn strings, derive, options and advance enum

--- src/bin/a14.rs
+++ src/bin/a14.rs
@@ -11,4 +11,35 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    name: String,
+    age: i32,
+    favorite_color: String,
+}
+
+
+fn print_details(name: &str, color: &str) {
+   println!("name: {:?}, favorite color: {:?}", name, color)
+} 
+fn main() {
+
+        let store = vec![
+        Person {
+            name: "victor".to_owned(),
+            age: 9,
+            favorite_color: "blue".to_owned(),
+        },
+        Person {
+            name: "victor".to_owned(),
+            age: 10,
+            favorite_color: "blue".to_owned(),
+        }
+    ];
+
+    for item in store {
+        if item.age >= 10 {
+            print_details(&item.name, &item.favorite_color);
+        }
+    }
+
+}
